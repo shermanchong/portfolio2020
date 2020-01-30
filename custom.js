4381106f949b68832a69a2d3178269a404830fc4
ShermanBCIT <sherman.cm.chong@gmail.com>
2020-01-29 16:11:04 -0800

    added digital assets page, fixed photography sizing issue, compressed image size for faster load

---
+++ custom.js
@@ -0,0 +1,27 @@
+
+M.AutoInit();
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true,
+    dist: 0,
+    duration: 100
+  });
+
+
+var body = document.querySelector(".body");
+
+
+function color(item){
+  
+  item.style.transform = "scale(1)";
+  body.style.backgroundColor= "white";
+
+}
+
+function grey(item){
+  item.style.transform = "scale(1.2)";
+  body.style.backgroundColor= "rgb(150,166,102,50%)";
+}
+
+
--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,25 @@
 M.AutoInit();
 
 var instance = M.Carousel.init({
-    fullWidth: true,
-    dist: 75,
-    duration: 100,
-    indicators: true,
-  });
+  fullWidth: true,
+  dist: 75,
+  duration: 00,
+  indicators: true,
+  
+});
+
+
+var body = document.querySelector(".body");
+
+
+function color(item){
+  
+  item.style.transform = "scale(1)";
+  body.style.backgroundColor= "white";
+
+}
+
+function grey(item){
+  item.style.transform = "scale(1.2)";
+  body.style.backgroundColor= "rgb(150,166,102,50%)";
+}
